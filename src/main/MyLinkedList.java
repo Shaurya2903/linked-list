725c64bf09c29be753666e168b5a49037dcf2059
shauryakaushal <shauryakaushal47@gmail.com>
2019-02-20 11:38:05 +0530

    implementation to current size

--- src/main/MyLinkedList.java
+++ src/main/MyLinkedList.java
@@ -1,19 +1,36 @@
 package main;
 
+import javafx.scene.Node;
+
 public class MyLinkedList {
+    private final Object head;
+    private final Object tail;
+    private int currentsize;
+
     //private Node head;
    // private Node tail;
     private int currentsize(){
         //r//eturn currentsize;
         return 0;
     }
-    public MyLinkedList(){
-      //  this.head=null;
-        //this.tail=null;
-        //c//urrentsize=0;
-    }
+
 
     public static void displayList(MyLinkedList linkedList) {
         MyLinkedList linkedList1 = new MyLinkedList();
     }
-}
+
+
+    public void setcurrentsize(int currentsize) {
+        this.currentsize = currentsize;
+    }
+    public MyLinkedList(){
+        this.head=null;
+        this.tail=null;
+        currentsize=0;
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+       // Node node = new Node(data: 10, next: null);
+    }
+    }
