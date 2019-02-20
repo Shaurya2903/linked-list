ce5bb2fc9d9afa55c8cf754f0c9dccc75180bcd9
shauryakaushal <shauryakaushal47@gmail.com>
2019-02-20 11:28:23 +0530

    node removed errors

--- src/main/MyLinkedList.java
+++ src/main/MyLinkedList.java
@@ -1,15 +1,16 @@
 package main;
 
 public class MyLinkedList {
-    private Node head;
-    private Node tail;
+    //private Node head;
+   // private Node tail;
     private int currentsize(){
-        return currentsize;
+        //r//eturn currentsize;
+        return 0;
     }
     public MyLinkedList(){
-        this.head=null;
-        this.tail=null;
-        currentsize=0;
+      //  this.head=null;
+        //this.tail=null;
+        //c//urrentsize=0;
     }
 
     public static void displayList(MyLinkedList linkedList) {
