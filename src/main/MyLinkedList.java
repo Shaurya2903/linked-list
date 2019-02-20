e4fa98025b473d39a4b0b3592c570c9e665aaf2a
shauryakaushal <shauryakaushal47@gmail.com>
2019-02-20 11:24:06 +0530

    Node

---
+++ src/main/MyLinkedList.java
@@ -0,0 +1,18 @@
+package main;
+
+public class MyLinkedList {
+    private Node head;
+    private Node tail;
+    private int currentsize(){
+        return currentsize;
+    }
+    public MyLinkedList(){
+        this.head=null;
+        this.tail=null;
+        currentsize=0;
+    }
+
+    public static void displayList(MyLinkedList linkedList) {
+        MyLinkedList linkedList1 = new MyLinkedList();
+    }
+}
